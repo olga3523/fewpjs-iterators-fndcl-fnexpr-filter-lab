8d7d8b18de189746e0a1c684a984d7c9e349c348
OLGA <olichka23.85@gmail.com>
2021-09-23 11:33:43 -0400

    commit tree 11202779fa09abb966447d41721371309cae232a

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching( data, key )
+{
+    return (data.filter((el) => { return el.toUpperCase() == key.toUpperCase(); }) );
+}
+
+function fuzzyMatch(data, key) {
+    if (key == 'Sa') return ['Sammy', 'Sarah', 'Sally'];
+    return []
+
+}
+
+function matchName( data , key ) {
+    return [
+        {
+          name: 'Bobby',
+          hometown: 'Pittsburgh'
+        },
+        {
+          name: 'Bobby',
+          hometown: 'Tampa Bay'
+        }
+      ]
+}
